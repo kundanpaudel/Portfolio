aee6b79eeb15eeda11ba850ad104fae1484c8073
Kundan Paudel <paudelkundan@gmail.com>
2023-09-06 15:30:07 -0600

    Header part finished with logo+portrait+buttons+effects

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,8 +1,30 @@
 import './header.css'
-
+import data from './data'
+import HeaderImage from '../../assets/kpheader.jpg'
 const header = () => {
   return (
-    <header id="header"><h2>Header</h2></header>
+    <header id="header">
+      <div className='container header__container'>
+        <div className="header__profile">
+          <img src={HeaderImage} alt='Portrait' />
+        </div>
+        <h3>Kundan Paudel</h3>
+        <p>Coding enthusiast pursuing excellence in computer science. 
+          Crafting innovative solutions with precision and a passion for learning.
+        </p>
+        <div className="header__contact">
+          <a href = '#contact' className='btn primary'>Reach Me!</a>
+          <a href = '#portfolio' className='btn light'>My Projects</a>
+        </div>
+        <div className="header__social">
+          {
+            data.map(item=> <a key={item.id} href={item.link} target="_blank" rel="noopener noreferrer">
+              {item.icon}
+            </a>)
+          }
+        </div>
+      </div>
+    </header>
   )
 }
 
---
+++ src/sections/header/data.js
@@ -0,0 +1,10 @@
+import {AiOutlineLinkedin} from 'react-icons/ai'
+import {AiFillGithub} from 'react-icons/ai'
+
+
+const data = [
+    {id:1, link: 'https://linkedin.com', icon: <AiOutlineLinkedin/>},
+    {id:1, link: 'https://github.com', icon: <AiFillGithub/>}
+]
+
+export default data
