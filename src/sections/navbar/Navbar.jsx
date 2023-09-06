b7fbe810ca9728ff692517e69bb153a621310217
Kundan Paudel <paudelkundan@gmail.com>
2023-09-05 22:14:05 -0600

    Navbar fixed with icons and dark mode button.

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,8 +1,25 @@
 import './navbar.css'
+import Logo from '../../assets/KPLogo.png'
+import data from './data'
+import {MdDarkMode} from 'react-icons/md'
 
 const navbar = () => {
   return (
-    <nav><h2>Navbar</h2></nav>
+    <nav>
+      <div className='container nav__container'>
+        <a href="index.html" className='nav__logo'>
+          <img src={Logo} alt="Logo" />
+        </a>
+        <ul className='nav__menu'>
+          {
+            data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)
+          }
+        </ul>
+        <button id='dark__mode'>
+          <MdDarkMode/>
+        </button>
+      </div>
+    </nav>
   )
 }
 
